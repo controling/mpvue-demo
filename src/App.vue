f702f5650f56b4b495aa4e1be5834f9ac6b51aeb
Control <516458638@qq.com>
2018-06-14 01:57:04 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
 
 <script>
-//引入配置项
-import {get, showSuccess, showFail} from "./util";
-import qcloud from "wafer2-client-sdk"
-import config from "./config"
 
 export default {
   async created() {
-    let user = wx.getStorageSync('userinfo')
-    if(!user){
-      qcloud.setLoginUrl(config.loginUrl)
-      qcloud.login({
-        success: function(userinfo){
-          console.log('登录成功', userinfo)
-          showSuccess('登录成功')
-          wx.setStorageSync('userinfo', userinfo)
-        },
-        fail: function (err) {
-          showFail('登录失败')
-          console.log('登录失败', err)
-        }
-      })
-    }
-    
-
+    console.log('小程序启动了')
     // const res = await get('/weapp/demo')
     // console.log(res)
     // wx.request({
--- src/pages/me/Me.vue
+++ src/pages/me/Me.vue
@@ -5,31 +5,113 @@
             <p>{{userinfo.nickName}}</p>
         </div>
         <YearProgress></YearProgress>
-        <button @click="scanBook" class="btn">添加图书</button>
+        <button v-if='userinfo.openId' @click='scanBook' class='btn'>添加图书</button>
+        <button v-else open-type="getUserInfo" lang="zh_CN" class='btn' @getuserinfo="login">点击登录</button>
+
     </div>
 </template>
 <script>
-import YearProgress from '@/components/YearProgress'
+import qcloud from "wafer2-client-sdk";
+import YearProgress from "@/components/YearProgress";
+import { get, showSuccess, showFail } from "@/util";
+import config from "@/config";
 export default {
-  components:{
+  components: {
     YearProgress
   },
   data() {
     return {
-      userinfo: {}
+      userinfo: {
+        avatarUrl: "http://image.shengxinjing.cn/rate/unlogin.png",
+        nickName: ""
+      }
     };
   },
   methods: {
-    scanBook(){
+    scanBook() {
       wx.scanCode({
-        success: (res) => {
-          console.log(res)
+        success: res => {
+          console.log(res);
         }
-      })
+      });
+    },
+    login(e) {
+      const self = this;
+      //查看是否授权
+      wx.getSetting({
+        success: function(res) {
+          //授权信息里面有用户信息
+          if (res.authSetting["scope.userInfo"]) {
+            // 检查用户登录是否过期
+            wx.checkSession({
+              success: function(res) {
+                // 没过期
+                showSuccess("登录成功");
+              },
+              fail: function() {
+                //过期了 重新登录 先清除登录的状态
+                qcloud.clearSession();
+                //登录状态已过期, 需要重新登录
+                //登录需要的加密信息 ?
+                var options = {
+                  encryptedData: e.mp.detail.encryptedData,
+                  iv: e.mp.detail.iv,
+                  userinfo: e.mp.detail.userInfo
+                };
+                self.getWxLogin(options);
+              }
+            });
+          } else {
+            showFail("用户未授权");
+          }
+        }
+      });
+    },
+    getWxLogin: function({ encryptedData, iv, userinfo }) {
+      const self = this;
+      wx.login({
+        success: function(loginResult) {
+          console.log("登录结果", loginResult);
+          var loginParams = {
+            code: loginResult.code,
+            encryptedData: encryptedData,
+            iv: iv
+          };
+          qcloud.setLoginUrl(config.loginUrl);
+          qcloud.requestLogin({
+            loginParams,
+            success() {
+              //获取用户信息
+              qcloud.request({
+                url: config.userUrl,
+                login: true,
+                success(userRes) {
+                  showSuccess("登录成功");
+                  wx.setStorageSync("userinfo", userRes.data.data);
+                  self.userinfo = userRes.data.data;
+                },
+                fail(res) {
+                  showFail("登录失败", res);
+                }
+              });
+            },
+            fail(res) {
+              showFail("登录失败", res);
+            }
+          });
+        },
+        fail(res) {
+          showFail("登录失败", res);
+        }
+      });
     }
   },
-  created() {
-    this.userinfo = wx.getStorageSync("userinfo");
+  onShow() {
+    let userinfo = wx.getStorageSync("userinfo");
+    if (userinfo) {
+      this.userinfo = userinfo;
+    }
+    // console.log(this.userinfo)
   }
 };
 </script>
